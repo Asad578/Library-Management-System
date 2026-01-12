530d3721128f1dbcc313f1fb4f2f3f9742b56d1f
Asad578 <aabahmad234@gmail.com>
2026-01-12 15:28:09 +0500

    fix username issue in alert

--- core/views.py
+++ core/views.py
@@ -52,7 +52,7 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.success(request, f'Welcome back, {user.username}!')
+                messages.success(request, f'Welcome back, {user.get_full_name() or user.username}!')
                 return redirect('core:dashboard')
             else:
                 messages.error(request, 'Invalid username or password.')
